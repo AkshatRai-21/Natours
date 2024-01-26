c9412d028faccb878182b8956f7642bc927f0548
AkshatRai-21 <raiakshat007@gmail.com>
2024-01-26 06:52:02 +0530

    updating package.json

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,7 +1,11 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = require('stripe')(
+import Stripe from 'stripe';
+
+
+// const stripe = await loadStripe(process.env.STRIPE_PK)
+const stripe = new Stripe(
   'pk_test_51MywrFSJZpmRPYEJb5JCyGkjFB1aXn2arckVyMYG7ImF8aJHaTmcDzEV8iVlHJ1LFQoKE3o6T1KmywWp1s0NVpLR009QbtXt1p'
 );
 
@@ -11,9 +15,9 @@ export const bookTour = async tourId => {
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2) Create checkout form + chanre credit card
-    await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
-    });
+    await Stripe.redirectToCheckout({
+      sessionId: DataTransfer.sessionId
+     });
   } catch (err) {
     console.log(err);
     showAlert('error', err);
